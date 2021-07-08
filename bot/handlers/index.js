2b959cee66af6e5aa695e15ae014ce1063e79ecb
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-07-08 13:28:13 +0530

    Update index.js

--- bot/handlers/index.js
+++ bot/handlers/index.js
@@ -1,7 +1,7 @@
 const play = require("./play");
 const controls = require("./controls");
 const voiceChatEnded = require("./voice_chat_ended");
-const privateChat = require("./private");
+
 
 module.exports = (bot) => {
     bot.filter((ctx) => {
@@ -17,6 +17,5 @@ module.exports = (bot) => {
         return false;
     }).use(play);
     bot.filter((ctx) => ctx.chat?.type === "supergroup", controls);
-    bot.filter((ctx) => ctx.chat?.type === "private", privateChat);
     bot.use(voiceChatEnded);
 };
